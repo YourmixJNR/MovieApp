7f9e3d24fc64e251b9141c7866ea1b66adb4dfa3
YourmixJNR <radmakson@gmail.com>
2023-10-23 14:19:02 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import SearchPage from './pages/SearchPage';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <SearchPage />
-    </div>
+    <Router>
+      <div className="">
+        <div className="">
+        <Header />
+        <Routes>
+          <Route path="/" element={<SearchPage />} />
+          <Route path="" element />
+        </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-      
+      <h1>This is our Header</h1>
     </div>
   )
 }
