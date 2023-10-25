b978278fb91efd0749b7408b29bfc4e16bf15e87
YourmixJNR <radmakson@gmail.com>
2023-10-25 11:54:30 +0100

    funcs

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,11 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import SearchBar from '../components/SearchBar';
 
 const SearchPage = () => {
+    const [query, setQuery] = useState('');
     const [searchResult, setSearchResult] = useState(null);
 
+    const handleInputChange = (event) => {
+        setQuery(event.target.value);
+    };
+
+    useEffect(() => {
+        const queryTerm = localStorage.getItem("searchQuery");
+
+        if (queryTerm) {
+            setQuery(queryTerm);
+            handleSearch(queryTerm);
+        }
+    }, []);
+
     const handleSearch = async (query) => {
+        localStorage.setItem("searchQuery", query);
+
         try {
             const response = await fetch(`http://www.omdbapi.com/?s=${query}&apikey=${process.env.REACT_APP_API}`);
             const data = await response.json();
@@ -22,23 +37,31 @@ const SearchPage = () => {
 
     return (
         <div>
-            <SearchBar onSearch={handleSearch} />
-            {searchResult && searchResult.Search ? (
-                <div>
-                    {searchResult.Search.map((item, index) => (
-                        <div key={index}>
-                            <img
-                                src={item.Poster}
-                                alt=""
-                            />
-                            <p>{item.Year}</p>
-                            <Link to={`/${item.imdbID}`}>Details</Link>
-                        </div>
-                    ))}
-                </div>
-            ) : (
-                <p>No results yet</p>
-            )}
+            <input
+                type="text"
+                value={query}
+                onChange={handleInputChange}
+            />
+            <button onClick={() => handleSearch(query)}>Search</button>
+
+            <div>
+                {searchResult && searchResult.Search ? (
+                    <div>
+                        {searchResult.Search.map((item, index) => (
+                            <div key={index}>
+                                <img
+                                    src={item.Poster}
+                                    alt=""
+                                />
+                                <p>{item.Year}</p>
+                                <Link to={`/${item.imdbID}`}>Details</Link>
+                            </div>
+                        ))}
+                    </div>
+                ) : (
+                    <p>No results yet</p>
+                )}
+            </div>
         </div>
     );
 };
