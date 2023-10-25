652462c03b209e2cc2e7381bfda13ca79a1d06b0
YourmixJNR <radmakson@gmail.com>
2023-10-25 10:39:40 +0100

    console log results on DetailPgae

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom'
 
 const DetailPage = () => {
+
+  const { imdbID } = useParams();
+
+  const fetchDetails = async () => {
+    try {
+      const response = await fetch(`http://www.omdbapi.com/?i=${imdbID}&apikey=${process.env.REACT_APP_API}`);
+      const result = await response.json();
+      console.log(result);
+    } catch(error) {
+      console.log('Error:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchDetails()
+  }, []);
+
   return (
     <div>
-      
+      <h1>Hello World</h1>
     </div>
   )
 }
 
-export default DetailPage
+export default DetailPage;
