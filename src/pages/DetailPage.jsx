e5b62e52b1d275b4381d3747c174291d7ea3d5e2
YourmixJNR <radmakson@gmail.com>
2023-10-25 11:00:02 +0100

    doings

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom'
 
 const DetailPage = () => {
-
   const { imdbID } = useParams();
+  const [details, setDetails] = useState({});
 
   const fetchDetails = async () => {
     try {
       const response = await fetch(`http://www.omdbapi.com/?i=${imdbID}&apikey=${process.env.REACT_APP_API}`);
       const result = await response.json();
-      console.log(result);
+      setDetails(result);
     } catch(error) {
       console.log('Error:', error);
     }
@@ -22,7 +22,10 @@ const DetailPage = () => {
 
   return (
     <div>
-      <h1>Hello World</h1>
+      <img
+        src={details.Poster}
+        alt=""
+      />
     </div>
   )
 }
